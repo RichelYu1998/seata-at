8fc401ce39ca6bbec7cb0f71e220144ba882081c
980187223@qq.com <Syp950628>
2020-09-05 15:37:34 +0800

    account账户项目_控制台显示输出代码&细节优化

--- account/src/main/java/cn/tedu/controller/AccountController.java
+++ account/src/main/java/cn/tedu/controller/AccountController.java
@@ -15,6 +15,7 @@ public class AccountController {
     private AccountService accountService;
     @GetMapping("/decrease")
     public String decrease(Long userId, BigDecimal money){
+        log.info("用户ID为"+userId+";扣款金额为"+money);
         accountService.decrease(userId, money);
         return "用户账户扣减金额成功";
     }
