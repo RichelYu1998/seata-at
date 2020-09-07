ffff923afdd4c00e830602dea109043a986d9dd0
980187223@qq.com <Syp950628>
2020-09-07 17:25:48 +0800

    启动分支事务&order 的业务类中调用扣减账户金额

--- account/src/main/java/cn/tedu/service/AccountServiceImpl.java
+++ account/src/main/java/cn/tedu/service/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package cn.tedu.service;
 
 import cn.tedu.mapper.AccountMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -10,7 +11,7 @@ import java.math.BigDecimal;
 public class AccountServiceImpl implements AccountService {
     @Resource
     private AccountMapper accountMapper;
-
+    @Transactional
     @Override
     public void decrease(Long userId, BigDecimal money) {
         accountMapper.decrease(userId, money);
