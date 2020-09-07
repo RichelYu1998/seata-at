3411e3bb2464e34ef6e92a4f04aa36ed2f7a5629
980187223@qq.com <Syp950628>
2020-09-07 16:35:43 +0800

    order-parent 添加 seata 依赖&TC注册&创建 seata 数据源代理&启动全局事务

--- order/src/main/java/cn/tedu/service/OrderServiceImpl.java
+++ order/src/main/java/cn/tedu/service/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import cn.tedu.feign.AccountClient;
 import cn.tedu.feign.EasyIdGeneratorClient;
 import cn.tedu.feign.StorageClient;
 import cn.tedu.mapper.OrderMapper;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -21,6 +22,7 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private StorageClient storageClient;
 
+    @GlobalTransactional
     @Override
     public void create(Order order) {
         // 从全局唯一id发号器获得id
@@ -30,9 +32,10 @@ public class OrderServiceImpl implements OrderService {
         orderMapper.create(order);
 
         // 修改库存
-        storageClient.decrease(order.getProductId(), order.getCount());
+        //storageClient.decrease(order.getProductId(), order.getCount());
 
         // 修改账户余额
-        accountClient.decrease(order.getUserId(), order.getMoney());
+        //accountClient.decrease(order.getUserId(), order.getMoney());
+
     }
 }
