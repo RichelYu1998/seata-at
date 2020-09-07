bc60d51f168b2943f9e8b6dffab12f68bea3a603
980187223@qq.com <Syp950628>
2020-09-07 11:46:00 +0800

    order订单添加Feign，调用库存和账户服务

--- order/src/main/java/cn/tedu/service/OrderServiceImpl.java
+++ order/src/main/java/cn/tedu/service/OrderServiceImpl.java
@@ -1,21 +1,38 @@
 package cn.tedu.service;
 
 import cn.tedu.entity.Order;
+import cn.tedu.feign.AccountClient;
+import cn.tedu.feign.EasyIdGeneratorClient;
+import cn.tedu.feign.StorageClient;
 import cn.tedu.mapper.OrderMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Random;
+
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Resource
     private OrderMapper orderMapper;
+    @Resource
+    EasyIdGeneratorClient easyIdGeneratorClient;
+    @Resource
+    private AccountClient accountClient;
+    @Resource
+    private StorageClient storageClient;
+
     @Override
     public void create(Order order) {
-        // TODO: 从全局唯一id发号器获得id，这里暂时随机产生一个 orderId
-        Long orderId  = Long.valueOf(new Random().nextInt(Integer.MAX_VALUE));
+        // 从全局唯一id发号器获得id
+        Long orderId = easyIdGeneratorClient.nextId("order_business");
         order.setId(orderId);
+
         orderMapper.create(order);
+
+        // 修改库存
+        storageClient.decrease(order.getProductId(), order.getCount());
+
+        // 修改账户余额
+        accountClient.decrease(order.getUserId(), order.getMoney());
     }
 }
